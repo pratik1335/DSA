5a69bfc3c15be0863b4fac0cd24106cdd844ca3f
pratik1335 <pratikgupta1335@gmail.com>
2026-05-28 18:34:04 +0000

    use infinity to avoid -ve numbers bug

--- Arrays/03_SecondLargest.js
+++ Arrays/03_SecondLargest.js
@@ -3,9 +3,17 @@
 // Core skill : Tracking multiple variables correctly.
 
 function secondLargest(myArray){
-    let max = myArray[0];
-    let second_max = max;
+    // checking empty array case
+    if(!myArray.length){
+        return 0;
+    }
 
+    let max = myArray[0];
+
+    // Corner case
+    // let second_max = -1; // will not work for all the -ve numbers
+    let second_max = -Infinity; // will work for all the -ve and +ve numbers
+    
     for(let i = 1; i < myArray.length; i++){
         if(myArray[i] > max){
             second_max = max;
@@ -21,8 +29,9 @@ function secondLargest(myArray){
 
 // let arr = [4, 9, 9]
 // let arr = [4, 9, 9, 18, 34, 67, 32, 56, 43, 90, -100, 42]
-// let arr = [-4, -9, -9, -18, -34, -67, -32, -56, -43, -90, -100, -42]
-let arr = [-4, -9, -9, -18, -34]
+let arr = [-18, -34, -67, -32, -56, -43, -90, -100, -42]
+// let arr = [-4, -9, -9, -18, -34]
+// let arr = []
 
 console.log(secondLargest(arr));
 
